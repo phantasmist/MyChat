41d4dbea2b858c851dc8655baf0a411233aef5e2
phantasmist <62749624+phantasmist@users.noreply.github.com>
2021-10-07 17:52:02 +0900

    초기 커밋.

---
+++ MyChat/frmChat.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace MyChat
+{
+    partial class frmChat
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.tbClient = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbConnectPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbConnectIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.tbServerPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 517;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tbServer);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tbClient);
+            this.splitContainer2.Size = new System.Drawing.Size(517, 450);
+            this.splitContainer2.SplitterDistance = 225;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tbServer
+            // 
+            this.tbServer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbServer.Location = new System.Drawing.Point(3, 3);
+            this.tbServer.Multiline = true;
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(514, 219);
+            this.tbServer.TabIndex = 0;
+            // 
+            // tbClient
+            // 
+            this.tbClient.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClient.Location = new System.Drawing.Point(3, -1);
+            this.tbClient.Multiline = true;
+            this.tbClient.Name = "tbClient";
+            this.tbClient.Size = new System.Drawing.Size(514, 219);
+            this.tbClient.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbConnectPort);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Controls.Add(this.tbConnectIP);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(18, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(243, 202);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // tbConnectPort
+            // 
+            this.tbConnectPort.Location = new System.Drawing.Point(110, 69);
+            this.tbConnectPort.Name = "tbConnectPort";
+            this.tbConnectPort.Size = new System.Drawing.Size(119, 25);
+            this.tbConnectPort.TabIndex = 4;
+            this.tbConnectPort.Text = "9000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PORT";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(54, 135);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(160, 38);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // tbConnectIP
+            // 
+            this.tbConnectIP.Location = new System.Drawing.Point(110, 26);
+            this.tbConnectIP.Name = "tbConnectIP";
+            this.tbConnectIP.Size = new System.Drawing.Size(119, 25);
+            this.tbConnectIP.TabIndex = 1;
+            this.tbConnectIP.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Server IP";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStartServer);
+            this.groupBox1.Controls.Add(this.tbServerPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(54, 117);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(160, 38);
+            this.btnStartServer.TabIndex = 2;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // tbServerPort
+            // 
+            this.tbServerPort.Location = new System.Drawing.Point(110, 26);
+            this.tbServerPort.Name = "tbServerPort";
+            this.tbServerPort.Size = new System.Drawing.Size(119, 25);
+            this.tbServerPort.TabIndex = 1;
+            this.tbServerPort.Text = "9000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Port";
+            // 
+            // frmChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmChat";
+            this.Text = "MyChat";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.TextBox tbClient;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.TextBox tbServerPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbConnectIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbConnectPort;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyChat/frmChat.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyChat
+{
+    public partial class frmChat : Form
+    {
+        public frmChat()
+        {
+            InitializeComponent();
+        }
+        //global var.
+        Socket sock = null;
+        TcpClient tcp = null;
+        TcpListener listen = null;
+        Thread threadServer = null; //Connect request thread
+        Thread threadRead = null; //input string parse thread
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            listen = new TcpListener(int.Parse(tbServerPort.Text));
+            listen.Start();
+
+            threadServer = new Thread(ServerProcess);
+            threadServer.Start();
+
+            threadRead = new Thread(ReadProcess);
+            
+              
+        }
+
+        void ServerProcess() //Connect request thread
+        {
+            while (true)
+            {
+                if (listen.Pending())
+                {
+                    tcp = listen.AcceptTcpClient(); //세션 수립
+                    AddText("Connection from Client []\r\n");
+                    threadRead.Start();
+                }
+                Thread.Sleep(100); //while 문의 부담을 줄이기 위함
+            }
+        }
+
+        void ReadProcess() //thread process
+        {
+            NetworkStream ns = tcp.GetStream();
+            byte[] buf = new byte[512];
+            while (true)
+            {
+                int n = ns.Read(buf, 0, 512);
+                AddText(Encoding.Default.GetString(buf, 0, n));
+            }
+        }
+        // delegate func
+        delegate void cbAddText(string str, int i = 0);
+        void AddText(string str, int i = 0)
+        {
+            if(tbServer.InvokeRequired || tbClient.InvokeRequired)
+            {
+                cbAddText cb = new cbAddText(AddText);
+                object[] obj = { str, i };
+                Invoke(cb, obj);
+            }
+            else
+            {
+                if (i == 0)
+                {
+                    tbServer.Text += str;
+                }
+                else
+                {
+                    tbClient.Text += str;
+                }
+            }
+            
+        }
+    }
+}
